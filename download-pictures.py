c3355eec4eb080da1c5a6526906b1da009051ed2
smartlyc <smartlyc520@gmail.com>
2016-08-02 16:03:56 +0800

    添加多线程功能，但是还不成熟，可用性很差

--- download-pictures.py
+++ download-pictures.py
@@ -4,7 +4,13 @@ Created on Sun Jul 10 16:52:50 2016
 
 @author: SMARTLYC
 """
+'''没有用了
+global thread1
+global thread2 
 
+thread1 = 'Thread-1'
+thread2 = 'Thread-2'
+'''
 #import urllib
 
 import chardet
@@ -13,7 +19,13 @@ import urllib2
 
 import requests
 
+import logging
+
+import thread
+
 from bs4 import BeautifulSoup
+
+
 
 #方法1.设置运行时的默认编码,可以解决图片.jpg，UnicodeEncodeError: 'ascii' codec can't encode characters in position 35-36: ordinal not in range(128)
 #存在不足，部分网页图片存取依旧报错
@@ -27,7 +39,7 @@ def debug_log(info):
     
     path = './debug_log.txt'
     
-    print type(info).__name__
+    #print type(info).__name__
     
     # 判断编码类型是否为 unicode ,包含中文需要将unicode 转换为 GB2312格式
     if type(info).__name__=="unicode" :  # and or 须分清
@@ -36,9 +48,7 @@ def debug_log(info):
     # int dict 转 str  
     if type(info).__name__ == "int" or type(info).__name__ =='dict': # == 符号切记不可以使用 =
        info = str(info)  # int 转 str
-    
-    #info = info.decode('utf8')
-       
+          
     with open(path,'a+') as f:  # 'w' 仅仅只会保存1129这一条网址，其他的都被覆盖掉了，'a+' 追加模式
         f.write(info)
         f.write('\n')
@@ -49,11 +59,20 @@ def debug_log(info):
 #def download（）:
 def download(url): #必须要注意中英文括号的不同（），(),会提示语法无效
     
-    r = requests.get(url)
+    
+    try:
+        #print url
+        r = requests.get(url)
+        #print r    
+    except Exception as e:
+        #logging.exception(e)
+        logging_exception = str(logging.exception(e))
+        debug_log(logging_exception)
+        return
     
     code = r.status_code
     #code = str(code) # 传入debug_log()的参数必须为字符串
-    print code
+    #print code
     debug_log(code)    
     
     if r.status_code !=200:
@@ -63,21 +82,21 @@ def download(url): #必须要注意中英文括号的不同（），(),会提示
     
     filename = url.split('/')[-1].split('.jpg')[0]    
     #filename = url.split('/')[6].split('.jpg')[0]    将6换为-1是考验能力滴
-    print 'filename ='+filename,'type(filename).__name__='+type(filename).__name__   
+    #print 'filename ='+filename,'type(filename).__name__='+type(filename).__name__   
     # filename 中包含中文时编码格式为 utf-8
     filename = filename.encode("utf-8")    
-    print 'filename ='+filename,'type(filename).__name__='+type(filename).__name__   
+    #print 'filename ='+filename,'type(filename).__name__='+type(filename).__name__   
 
     
     #单独测试时能够解决汉字问题，但是在此项目中无法解决问题
     #filename = urllib.quote(filename) #转换为unicode格式
 
     target = '../pic/{}.jpg'.format(filename)
-    print 'target=',target
+    #print 'target=',target
     #target = './{}.jpg'.format(filename)    图片存储路径设置
     chardet_target = chardet.detect(target)
     #chardet_target = str(chardet_target)  # 传入debug_log()的参数必须为字符串
-    print chardet_target
+    #print chardet_target
     debug_log(chardet_target)    
     
     # print chardet.detect(target) 为 {'confidence': 0.7525, 'encoding': 'utf-8'}  需要转换为unicode编码  使用decode()
@@ -92,39 +111,48 @@ def download(url): #必须要注意中英文括号的不同（），(),会提示
     return
 
 
-# 主函数
-#  u'' 字符串在内存中编码格式为unicode  ,此处是否加 u 都可以
-urls = [u'http://news.swjtu.edu.cn/ShowNews-{}-0-1.shtml'.format(number) for number in range(840,12000)]
+def mainmaster(threadName,delay):
+    # 主函数
+    #  u'' 字符串在内存中编码格式为unicode  ,此处是否加 u 都可以
+    urls = [u'http://news.swjtu.edu.cn/ShowNews-{}-0-1.shtml'.format(number) for number in range(900,910 )]
+    
+    
+    for url in urls:
+    
+        print threadName,url
+        debug_log(url)            
+        
+        request =urllib2.Request(url)
+        
+        response =urllib2.urlopen(request)
+        
+        soup = BeautifulSoup(response)
+            
+        pics = soup.select('div > p > img')    #(' p > img')也可以
+        
+        #pics = soup.select('ul > li > a > img')
+        
+        for pic in pics:
+            src = pic.get('src')
+            src = src.replace(' ', '')  ##去除空格
+            
+            http = 'http://'
+            if http in src:#:标点符号非常非常容易忘记加，尤其是中间插入if else 语句时！！！
+               src = src
+            else:          #:标点符号非常非常容易忘记加，尤其是中间插入if else 语句时！！！ 
+               src = 'http://news.swjtu.edu.cn'+src
+    
+            print threadName,src
+            debug_log(src)        
+            
+            download(src) #使用函数之前需事先定义函数
 
 
-for url in urls:
+#  创建2个线程
 
-    print url
-    debug_log(url)            
-    
-    request =urllib2.Request(url)
-    
-    response =urllib2.urlopen(request)
-    
-    soup = BeautifulSoup(response)
-        
-    pics = soup.select('div > p > img')    #(' p > img')也可以
-    
-    #pics = soup.select('ul > li > a > img')
-    
-    for pic in pics:
-        src = pic.get('src')
-        src = src.replace(' ', '')  ##去除空格
-        
-        http = 'http://'
-        if http in src:#:标点符号非常非常容易忘记加，尤其是中间插入if else 语句时！！！
-           src = src
-        else:          #:标点符号非常非常容易忘记加，尤其是中间插入if else 语句时！！！ 
-           src = 'http://news.swjtu.edu.cn'+src
-
-        print src
-        debug_log(src)        
-        
-        download(src) #使用函数之前需事先定义函数
-        
-    
+try:
+   thread.start_new_thread(mainmaster,('Thread-1',1))
+   thread.start_new_thread(mainmaster,('Thread-2',2))
+except:
+   print "Error: unable to start thread"       
+   
